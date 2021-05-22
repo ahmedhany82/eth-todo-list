d84e1ae7bdf23419c388e826946c8d87e640c380
Ahmed Abdelsalam <ahmedabdelsalam82.de@gmail.com>
2021-05-22 15:46:15 +0200

    added tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,22 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    constructor() public {
+        creatTask("Learn Solidity");
+    }
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function creatTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
 
